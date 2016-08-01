392980cf24a7f12de8a4681d5521eac2f373d23c
rocity <hello@kvnpl.xyz>
2016-08-01 20:51:28 +0800

    Convert method to jQuery plugin

--- bgimg.js
+++ bgimg.js
@@ -1,13 +1,48 @@
-"use strict";
-jQuery(document).ready(function ($) {
-    $('.bgimg').each(function (i, o) {
-        var t   = $(this),
-            d   = t.data();
-        t
-            .css('background-image', 'url('+d.bgimg+')')
-            .css('background-color', d.bgimgColor)
-            .css('background-position', d.bgimgPosition)
-            .css('background-size', d.bgimgSize)
-            .css('background-repeat', d.bgimgRepeat);
-    });
-});
+(function ($) {
+    $.fn.bgimg = function (options) {
+        "use strict";
+
+        // Initialize default attributes
+        var settings = $.extend({
+            backgroundColor: "transparent",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "auto auto",
+            backgroundPosition: "50% 50%",
+        }, options);
+
+        // Loop through each matched element
+        this.each(function (i, o) {
+            var t   = $(this),
+                d   = t.data();
+
+            // Check if data attributes are set on the element
+            $.each(d, function (i, v) {
+                switch(i) {
+                    case 'bgimgColor':
+                        settings.backgroundColor = v;
+                        break;
+                    case 'bgimgRepeat':
+                        settings.backgroundRepeat = v;
+                        break;
+                    case 'bgimgSize':
+                        settings.backgroundSize = v;
+                        break;
+                    case 'bgimgPosition':
+                        settings.backgroundPosition = v;
+                        break;
+                }
+            })
+
+            // Set CSS rules for the element
+            $(this).css({
+                backgroundImage:    'url('+d.bgimg+')',
+                backgroundColor:    settings.backgroundColor,
+                backgroundRepeat:   settings.backgroundRepeat,
+                backgroundSize:     settings.backgroundSize,
+                backgroundPosition: settings.backgroundPosition
+            })
+            
+        });
+        return this;
+    }
+}(jQuery))
